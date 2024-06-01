0901ad564b14c2f388771301f102cff671fa30db
Ayushipoonia <ayushiponiabp@gmail.com>
2024-06-01 16:58:55 +0530

    solution of 2

--- Task2.cpp
+++ Task2.cpp
@@ -1,1 +1,25 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int num1;
+    int num2;
+    char oper;
+    cin>>num1;
+    cin>>num2;
+    cin>>oper;
+    if(oper=='+'){
+        cout<<num1+num2;
+    }
+    else if(oper=='-'){
+        cout<<num1-num2;
+    }
+    else if(oper=='*'){
+        cout<<num1*num2;
+    }
+    else if(oper=='/'){
+        cout<<num1/num2;
+    }
+    else{
+        cout<<"Invalid operator";
+    }
+}
