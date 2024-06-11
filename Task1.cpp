5b4681eff3442eb9ed15ef9c3f321e98a46d0fce
Ayushipoonia <143319757+Ayushipoonia@users.noreply.github.com>
2024-06-11 17:28:15 +0530

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int random_num=rand()%100+1;
+    int guess=0;
+    while(guess!=random_num){
+        cin>>guess;
+        if(guess<random_num){
+            cout<<"your guess is too low."<<endl;
+        }
+        else if(guess>random_num){
+            cout<<"your guess is too high"<<endl;
+        }
+        else{
+            cout<<"you are correct"<<endl;
+        }
+    }
+    return 0;
 
+}
